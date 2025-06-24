8fb468f4fda6c11ea965b663450339cf2dce0f96
Gatu-1548 <jorge.alfredo.mamani@gmail.com>
2025-06-24 16:38:53 -0400

    add api apointmenst

--- lib/utils/constantes.dart
+++ lib/utils/constantes.dart
@@ -1,3 +1,4 @@
 class Constantes {
-  static String uri = "https://nest-project-image-recognition.onrender.com";
+  static String uri =
+      "https://nest-project-image-recognition-si53.onrender.com";
 }
